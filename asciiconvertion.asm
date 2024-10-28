41cab9e86b6d141ea221b38c8c4b2c5bf8d7bd75
Ashiqur Rahman <imashiqe@gmail.com>
2024-10-28 16:51:11 +0600

    ASCII Conversion

---
+++ asciiconvertion.asm
@@ -0,0 +1,26 @@
+.model small
+.data
+.code
+main proc
+
+mov ah,1
+int 21h
+mov bl,al
+
+mov ah,1
+int 21h
+mov bh,al
+
+add bl,bh ;bl=bl+bh
+sub bl,48
+
+mov ah,2
+mov dl,bl
+
+int 21h
+
+exit:
+mov ah,4ch
+int 21h
+main endp
+end main
